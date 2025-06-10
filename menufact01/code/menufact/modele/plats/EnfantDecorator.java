d6476234512dbda5d446fc986b9251620c275346
Mehdi <amrm1702@usherbrooke.ca>
2025-06-09 21:12:51 -0400

    implementation decorateur et modification factory

---
+++ menufact01/code/menufact/modele/plats/EnfantDecorator.java
@@ -0,0 +1,22 @@
+package menufact.modele.plats;
+
+public class EnfantDecorator extends PlatDecorator {
+    private double proportion;
+
+    public EnfantDecorator(Plat plat, double proportion) {
+        super(plat);
+        this.proportion = proportion;
+    }
+
+    @Override
+    public double getPrix() {
+        return super.getPrix() * proportion;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", proportion=" + proportion;
+    }
+
+
+}
---
+++ menufact01/code/menufact/modele/plats/SanteDecorateur.java
@@ -0,0 +1,19 @@
+package menufact.modele.plats;
+
+public class SanteDecorateur extends PlatDecorator {
+    private double kcal;
+    private double chol;
+    private double gras;
+
+    public SanteDecorateur(Plat plat, double kcal, double chol, double gras) {
+        super(plat);
+        this.kcal = kcal;
+        this.chol = chol;
+        this.gras = gras;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", kcal=" + kcal + ", chol=" + chol + ", gras=" + gras;
+    }
+}
